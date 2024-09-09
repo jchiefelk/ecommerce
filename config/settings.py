bd26c355a42fc1d1deecd5735abeb0cdf3e18bed
Jackson Chief Elk <jackson.chiefelk@schrodinger.com>
2024-09-09 09:16:01 -0700

    Add Procfile

--- config/settings.py
+++ config/settings.py
@@ -5,14 +5,11 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 from decouple import config
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DEBUG")
 
 ALLOWED_HOSTS = []
 
