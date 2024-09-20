66594ab7e73dffaf130b2f4b520e338338621fe8
Jackson Chief Elk <jackson.chiefelk@schrodinger.com>
2024-09-20 11:40:33 -0600

    Update heroku settings

--- config/settings.py
+++ config/settings.py
@@ -141,15 +141,16 @@ if IS_HEROKU_APP:
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
     AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
     # s3 static settings
-    AWS_LOCATION = 'static'
-    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
+    # AWS_LOCATION = 'static'
+    # STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
+    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/'
     STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
     MEDIA_URL = f"https://{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/"
     MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 else:
     STATIC_URL = '/staticfiles/'
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-    MEDIA_URL = "/media/"
+    MEDIA_URL = "/"
     MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
@@ -157,7 +158,6 @@ DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 # MEDIA_URL = '/mediafiles/'
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
-
 
 STORAGES = {
     # Enable WhiteNoise's GZip and Brotli compression of static assets:
