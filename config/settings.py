77dedccafb7c527a05b237676adaebbade648c0a
Jackson Chief Elk <jackson.chiefelk@schrodinger.com>
2024-09-20 11:50:14 -0600

    Update heroku settings

--- config/settings.py
+++ config/settings.py
@@ -138,7 +138,7 @@ if IS_HEROKU_APP:
     # AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
 
     AWS_DEFAULT_ACL = 'public-read'
-    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.us-east-2.amazonaws.com'
     AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
     # s3 static settings
     # AWS_LOCATION = 'static'
